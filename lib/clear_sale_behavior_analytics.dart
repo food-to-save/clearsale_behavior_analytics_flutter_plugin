bd887a0846756f88f4995263d3b3b1a6d96c90f7
rullyalvess-foodtosave <rully.alves@foodtosave.com>
2023-09-11 11:06:44 -0300

    refactor: stop methods android only if platform is iOs

--- lib/clear_sale_behavior_analytics.dart
+++ lib/clear_sale_behavior_analytics.dart
@@ -1,14 +1,37 @@
+import 'dart:io';
+
 import 'clear_sale_behavior_analytics_platform_interface.dart';
 
 class ClearSaleBehaviorAnalytics {
-  
-  Future<void> start(String appId) async => ClearSaleBehaviorAnalyticsPlatform.instance.start(appId);
+  Future<void> start(String appId) async {
+    if (Platform.isIOS) {
+      return;
+    }
+    return ClearSaleBehaviorAnalyticsPlatform.instance.start(appId);
+  }
 
-  Future<void> stop() => ClearSaleBehaviorAnalyticsPlatform.instance.stop();
+  Future<void> stop() async {
+    if (Platform.isIOS) {
+      return;
+    }
+    return ClearSaleBehaviorAnalyticsPlatform.instance.stop();
+  }
 
-  Future<void> blockGeolocation() => ClearSaleBehaviorAnalyticsPlatform.instance.blockGeolocation();
+  Future<void> blockGeolocation() async {
+    if (Platform.isIOS) {
+      return;
+    }
+    return ClearSaleBehaviorAnalyticsPlatform.instance.blockGeolocation();
+  }
 
-  Future<void> blockAppList() => ClearSaleBehaviorAnalyticsPlatform.instance.blockAppList();
+  Future<void> blockAppList() async {
+    if (Platform.isIOS) {
+      return;
+    }
+    return ClearSaleBehaviorAnalyticsPlatform.instance.blockAppList();
+  }
 
-  Future<String?> collectInformation() => ClearSaleBehaviorAnalyticsPlatform.instance.collectInformation();
+  Future<String?> collectInformation() {
+    return ClearSaleBehaviorAnalyticsPlatform.instance.collectInformation();
+  }
 }
